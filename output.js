c3aecda6e7a79b37a048846a024efb4338e89310
Sven Beisiegel <162966719+svenbeisiegel@users.noreply.github.com>
2025-08-18 13:31:53 +0200

    Update output.js

--- output.js
+++ output.js
@@ -1,24 +1,26 @@
 const util = require('node:util');
 
-let payload = '';
+const payload = {}
 
 function toStringSafe(value) {
-  return typeof value === "string" ? value : util.inspect(value, { depth: null, colors: false });
+    return typeof value === "string" ? value : util.inspect(value, { depth: null, colors: false });
 }
 
 module.exports = function (RED) {
     function TextDisplayNode(config) {
         RED.nodes.createNode(this, config);
         const node = this;
+        payload[node.id] = '';
 
         node.on('input', function (msg) {
-            payload = msg.payload || '';
-            node.status({fill:'green',shape:'dot',text:`Received ${toStringSafe(payload).length} characters`});
+            payload[node.id] = msg.payload || '';
+            node.status({fill:'green',shape:'dot',text:`Received ${toStringSafe(payload[node.id]).length} characters`});
             node.send(msg);
         });
 
-        RED.httpAdmin.get("/osem-out/get-data", RED.auth.needsPermission("osem-out.read"), function(req, res) {
-            res.end(toStringSafe(payload));
+        RED.httpAdmin.get(`/osem-out/${node.id}`, RED.auth.needsPermission("osem-out.read"), function(req, res) {
+            console.log(`GET /osem-out/${node.id}`);
+            res.end(toStringSafe(payload[node.id]));
         });
     
         node.on('close', function() {
